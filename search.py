9463501df0e0bf1d0279582f94594ac094667191
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-03-11 23:49:18 +0900

    スクレイピング処理のインデントを修正

--- search.py
+++ search.py
@@ -28,32 +28,32 @@ def main(kwords):
         ET.SubElement(channel, "description").text = description
         ET.SubElement(channel, "link").text = "https://example.com"
 
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.get(url, headers=headers)
-        html_content = response.text
-        print(f"Response status code: {response.status_code}")
-        
-        events_found = 0
-        channel = root.find("channel")
-        
-        found_events = event_pattern.findall(html_content)
-        
-        for match in found_events:
-            event_html = match
-            date = re.search(r'title="(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)"', event_html).group(1)
-            title = re.search(r' alt="(.*?)" />', event_html).group(1)
-            link = re.search(r'<p class="event_title"><a class="url summary" href="(https:\/\/[a-zA-Z0-9\-\.\/]+)"', event_html).group(1)
+    # スクレイピングを実行
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    html_content = response.text
+    print(f"Response status code: {response.status_code}")
 
-            print(f"Scraped Event: {title}, {link}")  # タイトルとリンクを出力
-            
-            if link in existing_links:
-                continue
+    # イベントリスト取得
+    channel = root.find("channel")
+    found_events = event_pattern.findall(html_content)
+    
+    for match in found_events:
+        event_html = match
+        date = re.search(r'title="(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)"', event_html).group(1)
+        title = re.search(r' alt="(.*?)" />', event_html).group(1)
+        link = re.search(r'<p class="event_title"><a class="url summary" href="(https:\/\/[a-zA-Z0-9\-\.\/]+)"', event_html).group(1)
+
+        print(f"Scraped Event: {title}, {link}")  # タイトルとリンクを出力
         
-            # RSSのitem要素を追加
-            new_item = ET.SubElement(channel, "item")
-            ET.SubElement(new_item, "title").text = title
-            ET.SubElement(new_item, "link").text = link
-            ET.SubElement(new_item, "pubDate").text = date
+        if link in existing_links:
+            continue
+    
+        # RSSのitem要素を追加
+        new_item = ET.SubElement(channel, "item")
+        ET.SubElement(new_item, "title").text = title
+        ET.SubElement(new_item, "link").text = link
+        ET.SubElement(new_item, "pubDate").text = date
 
     xml_str = ET.tostring(root)
     # 不正なXML文字を取り除く
