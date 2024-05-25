63e070cdd4f6ba88e8aa687398a2da041a2c0b09
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-05-25 17:18:43 +0900

    add miyagi

--- src/search.py
+++ src/search.py
@@ -68,7 +68,7 @@ def search(channel, kwords):
 
 
 if __name__ == "__main__":
-    kwords = ["hokkaido", "aomori", "iwate", "online"]
+    kwords = ["hokkaido", "aomori", "iwate", "miyagi", "online"]
 
     for kword in kwords:
         # RSSファイルの読み込み
