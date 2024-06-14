d87cb1f35586fc281458b535d9573fb8392abb9b
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-06-14 17:18:43 +0900

    add ibaraki

--- src/search.py
+++ src/search.py
@@ -68,7 +68,7 @@ def search(channel, kwords):
 
 
 if __name__ == "__main__":
-    kwords = ["hokkaido", "aomori", "iwate", "miyagi", "akita", "yamagata", "fukushima", "ibaraki", "online"]
+    kwords = ["hokkaido", "aomori", "iwate", "miyagi", "akita", "yamagata", "fukushima", "ibaraki", "tochigi", "online"]
 
     for kword in kwords:
         # RSSファイルの読み込み
